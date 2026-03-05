cc78f6a9eb019975ecd16cbb9974a1c9d132a1a3
chris <christosste86@gmail.com>
2026-03-05 12:14:01 +0100

    add ingredients-service

---
+++ Ingredients-service/src/main/java/com/example/Ingredients_service/IngredientsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ingredients_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientsServiceApplication.class, args);
+	}
+
+}
---
+++ Ingredients-service/src/test/java/com/example/Ingredients_service/IngredientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ingredients_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
