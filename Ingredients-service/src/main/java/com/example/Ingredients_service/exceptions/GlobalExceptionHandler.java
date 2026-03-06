3c3402bd65081b279c71d3b520a6cd5f9d37d84f
chris <christosste86@gmail.com>
2026-03-06 08:34:17 +0100

    create controllers and HandleExceptions

---
+++ Ingredients-service/src/main/java/com/example/Ingredients_service/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.Ingredients_service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex){
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
