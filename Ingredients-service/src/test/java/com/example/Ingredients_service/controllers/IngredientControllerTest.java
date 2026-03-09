05adaf3b928d1bffca40332b529504dfe97ed18e
chris <christosste86@gmail.com>
2026-03-09 08:03:07 +0100

    back changes

--- Ingredients-service/src/test/java/com/example/Ingredients_service/controllers/IngredientControllerTest.java
+++ Ingredients-service/src/test/java/com/example/Ingredients_service/controllers/IngredientControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(IngredientController.class)
+@ActiveProfiles("test")
 class IngredientControllerTest {
 
     @Autowired
