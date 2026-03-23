b39f50b46e444859085c9f822a036c7fa9207408
chris <christosste86@gmail.com>
2026-03-23 18:26:04 +0100

    create CategoryControllerTest getCategoryById()

--- recipe-service/src/test/java/com/example/recipe_service/controllers/CategoryControllerTest.java
+++ recipe-service/src/test/java/com/example/recipe_service/controllers/CategoryControllerTest.java
@@ -1,7 +1,10 @@
 package com.example.recipe_service.controllers;
 
 import com.example.recipe_service.dtos.category.CategoryCreateRequestDto;
+import com.example.recipe_service.dtos.category.CategoryResponseDto;
+import com.example.recipe_service.models.Category;
 import com.example.recipe_service.services.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +14,11 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CategoryController.class)
@@ -26,6 +30,23 @@ class CategoryControllerTest {
 
     @MockitoBean
     private CategoryService categoryService;
+    private Category category;
+    private CategoryResponseDto categoryResponseDto;
+
+    @BeforeEach
+    void setup(){
+        this.category = Category.builder()
+                .id(1)
+                .name("Category")
+                .imgUrl("url")
+                .build();
+
+        this.categoryResponseDto = CategoryResponseDto.builder()
+                .id(this.category.getId())
+                .name(this.category.getName())
+                .imgUrl(this.category.getImgUrl())
+                .build();
+    }
 
     @Test
     void createCategory() throws Exception {
@@ -93,11 +114,30 @@ class CategoryControllerTest {
     }
 
     @Test
-    void getCategories() {
+    void getCategories() throws Exception {
+        //Arrange
+        List<CategoryResponseDto> categories = List.of(this.categoryResponseDto);
+        //Mock
+        when(categoryService.getCategories()).thenReturn(categories);
+        //Preform Get
+        mockMvc.perform(get("/api/recipe-categories"))
+                .andExpect(status().isOk());
+        //Verify
+        verify(categoryService).getCategories();
     }
 
     @Test
-    void getCategoryById() {
+    void getCategoryById() throws Exception {
+        //Arrange
+        Integer categoryId = this.category.getId();
+        //Mock
+        when(categoryService.getCategoryResponseDtoById(categoryId))
+                .thenReturn(this.categoryResponseDto);
+        //Preform get
+        mockMvc.perform(get("/api/recipe-categories/{categoryId}", categoryId))
+                .andExpect(status().isOk());
+        //Verify
+        verify(categoryService).getCategoryResponseDtoById(categoryId);
     }
 
     @Test
