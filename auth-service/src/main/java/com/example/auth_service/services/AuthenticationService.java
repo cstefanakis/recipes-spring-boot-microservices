77d35554e6f9fd36c074772313a6e03494da9214
chris <christosste86@gmail.com>
2026-04-08 11:41:57 +0200

    update auth-service and role

---
+++ auth-service/src/main/java/com/example/auth_service/services/AuthenticationService.java
@@ -0,0 +1,64 @@
+package com.example.auth_service.services;
+
+import com.example.auth_service.dtos.LoginUserDto;
+import com.example.auth_service.dtos.RegisterUserDto;
+import com.example.auth_service.models.Role;
+import com.example.auth_service.models.User;
+import com.example.auth_service.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder, JwtService jwtService
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = User.builder()
+                .fullName(input.getFullName())
+                .email(input.getEmail())
+                .password(passwordEncoder.encode(input.getPassword()))
+                .role(Role.USER)
+                .build();
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+        
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", user.getRole());
+
+        return user;
+    }
+}
