9071d2b2a0ab83e5acbccc4b7319c76c93671b6a
chris <christosste86@gmail.com>
2026-04-13 19:11:32 +0200

    create jwt tests

--- Ingredients-service/src/test/java/com/example/Ingredients_service/IngredientsServiceApplicationTests.java
+++ Ingredients-service/src/test/java/com/example/Ingredients_service/IngredientsServiceApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.Ingredients_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "security.jwt.secret-key=test-secret-key"})
 class IngredientsServiceApplicationTests {
 
 	@Test
